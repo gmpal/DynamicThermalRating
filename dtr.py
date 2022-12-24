d114fea4a2928c86e72edd3330b5560554efef96
gmpal <g.m.pal95@gmail.com>
2022-12-24 18:57:43 +0100

    Parametric added

--- dtr.py
+++ dtr.py
@@ -3,14 +3,147 @@ import pandas as pd
 import multiprocessing as mp
 from scipy.stats import gaussian_kde
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+
+from adapt.instance_based import * 
+from adapt.parameter_based import *
+from sklearn.base import clone
 
 
 class DTR():
 
-    def __init__(self, data, n_jobs=5):
+    def __init__(self, data, n_jobs=5, random_state=42):
             
             self.data = data
             self.distances_df = None
+            self.n_jobs = n_jobs
+            self.random_state = random_state
+
+    def instance_based_transfer(self, source_sensor_id, target_sensor_id, inputs, output, target_num_available_days = 5, target_num_test_days=30, regressor = None, sliding_window = False, metric = mean_squared_error, estimators_tradaboost = 20, verbose = 1):        
+        source = self.data.loc[self.data.id == source_sensor_id]
+        target = self.data.loc[self.data.id == target_sensor_id]
+        
+        days_in_target = target['datetime'].dt.date.nunique()
+        if days_in_target < (target_num_available_days + target_num_test_days):
+            raise ValueError("The target sensor does not have enough data to perform the experiment with the specified parameters.")
+
+        testing_days = target['datetime'].dt.date.unique()[-target_num_test_days:] 
+        target_test = target.loc[target['datetime'].dt.date.between(testing_days[0], testing_days[-1])]
+
+
+        Xs = source[inputs] #source inputs
+        ys = source[output] #source output
+        X_test = target_test[inputs] #target inputs
+        y_test = target_test[output] #target output
+        y_IEEE_738 = target_test['Conductor Temp. estimated by dyn_IEEE_738 (t+1) [°C]']
+
+        # Get the unique dates in the target data
+        dates = target['datetime'].dt.date.unique()
+        
+        if sliding_window:
+            # Loop through the dates, extracting available data in a sliding window fashion
+            for i in range(len(dates) - target_num_test_days - target_num_available_days + 1):
+                # Extract available data for the target sensor
+                available_days = dates[i:i+target_num_available_days]
+                target_available = target.loc[target['datetime'].dt.date.between(available_days[0], available_days[-1])]
+                X_t_available = target_available[inputs]
+                y_t_available = target_available[output]
+
+                regressor_source = clone(regressor)
+                regressor_mix = clone(regressor)
+                model = TrAdaBoostR2(regressor, n_estimators=estimators_tradaboost, random_state=self.random_state, verbose=verbose)
+                model.fit(Xs, ys, X_t_available, y_t_available)
+                
+                
+                regressor_source.fit(Xs, ys)
+                regressor_mix.fit(pd.concat([Xs,X_t_available]), pd.concat([ys,y_t_available]))
+                results = pd.DataFrame(columns=['day', 'Beats IEEE?', 'Beats baseline?', 'Beats mix?'])
+                # Loop over the test days
+                for day in testing_days:
+                    # Extract test data for the current day
+                    target_test = target.loc[target['datetime'].dt.date == day]
+                    X_test = target_test[inputs]
+                    y_test = target_test[output]
+                    y_hat_trAdaBoost = model.predict(X_test)
+                    y_hat_baseline = regressor_source.predict(X_test)
+                    y_hat_mix = regressor_mix.predict(X_test)
+                    y_IEEE_738 = target_test['Conductor Temp. estimated by dyn_IEEE_738 (t+1) [°C]']
+                    results.loc[len(results)] = [day, mean_squared_error(y_test, y_hat_trAdaBoost) < mean_squared_error(y_test, y_IEEE_738), mean_squared_error(y_test, y_hat_trAdaBoost) < mean_squared_error(y_test, y_hat_baseline), mean_squared_error(y_test, y_hat_trAdaBoost) < mean_squared_error(y_test, y_hat_mix)]
+                
+                return results
+                break
+
+        else:
+            #TODO: implement the non-sliding window version
+            pass    
+
+    def parameter_based_transfer(self, source_sensor_id, target_sensor_id, inputs, output, target_num_available_days = 5, target_num_test_days=30, regressor = None, sliding_window = False, metric = mean_squared_error, verbose = 1, epochs = 100):        
+        source = self.data.loc[self.data.id == source_sensor_id]
+        target = self.data.loc[self.data.id == target_sensor_id]
+
+
+        
+        days_in_target = target['datetime'].dt.date.nunique()
+        if days_in_target < (target_num_available_days + target_num_test_days):
+            raise ValueError("The target sensor does not have enough data to perform the experiment with the specified parameters.")
+
+        testing_days = target['datetime'].dt.date.unique()[-target_num_test_days:] 
+        target_test = target.loc[target['datetime'].dt.date.between(testing_days[0], testing_days[-1])]
+
+
+        Xs = source[inputs] #source inputs
+        ys = source[output] #source output
+        X_test = target_test[inputs] #target inputs
+        y_test = target_test[output] #target output
+        y_IEEE_738 = target_test['Conductor Temp. estimated by dyn_IEEE_738 (t+1) [°C]']
+
+        # Get the unique dates in the target data
+        dates = target['datetime'].dt.date.unique()
+        
+        if sliding_window:
+            # Loop through the dates, extracting available data in a sliding window fashion
+            for i in range(len(dates) - target_num_test_days - target_num_available_days + 1):
+                # Extract available data for the target sensor
+                available_days = dates[i:i+target_num_available_days]
+                target_available = target.loc[target['datetime'].dt.date.between(available_days[0], available_days[-1])]
+                X_t_available = target_available[inputs]
+                y_t_available = target_available[output]
+                
+                # scaler = StandardScaler()
+                
+                # Xs = scaler.fit_transform(Xs)
+                # X_t_available = scaler.transform(X_t_available)
+                # X_test = scaler.transform(X_test)
+
+                src_model = RegularTransferNN(loss="mse", random_state=self.random_state, verbose=verbose)
+                src_model.fit(Xs, ys, epochs=epochs, verbose=verbose)
+                
+                model = RegularTransferNN(src_model.task_, loss="mse",random_state=self.random_state, verbose=verbose)
+                model.fit(X_t_available, y_t_available, epochs=epochs, verbose=verbose)
+                
+                results = pd.DataFrame(columns=['day', 'Beats IEEE?', 'Beats baseline?'])
+                # Loop over the test days
+                for day in testing_days:
+                    # Extract test data for the current day
+                    target_test = target.loc[target['datetime'].dt.date == day]
+                    X_test = target_test[inputs]
+                    y_test = target_test[output]
+                    y_hat_transfer = model.predict(X_test)
+                    y_hat_baseline = src_model.predict(X_test)
+                    y_IEEE_738 = target_test['Conductor Temp. estimated by dyn_IEEE_738 (t+1) [°C]']
+                    results.loc[len(results)] = [day, mean_squared_error(y_test, y_hat_transfer) < mean_squared_error(y_test, y_IEEE_738), mean_squared_error(y_test, y_hat_transfer) < mean_squared_error(y_test, y_hat_baseline)]
+                
+                return results
+                break
+
+        else:
+            #TODO: implement the non-sliding window version
+            pass   
+
+
+        
 
 
     def compute_sensors_distance(self):
@@ -86,3 +219,8 @@ class DTR():
 
     def load_distances(self, path):
         self.distances_df = pd.read_csv(path)
+
+    def sample_data(self, ratio = 0.1):
+        for sensor in self.data.id.unique():
+            for day in self.data.loc[self.data.id == sensor].datetime.dt.date.unique():
+                self.data.loc[(self.data.id == sensor) & (self.data.datetime.dt.date == day)] = self.data.loc[(self.data.id == sensor) & (self.data.datetime.dt.date == day)].sample(frac=ratio, random_state=self.random_state)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from dtr import DTR
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import Ridge
+import time
 
-data = pd.read_csv('./data.zip',index_col=0)
-data.columns
+data = pd.read_csv('./data.zip',index_col=0, infer_datetime_format=True, parse_dates=['datetime'])
+inputs = ['Wind Speed [m/s]', 'Arranged Wind Dir [°]', 'Air temp [°C]', 'Humidity [%]', 'Sun irradiance thermal flow absorbed by the conductor.[W/m]', 'Current flow [A]']
+output = ['Actual Conductor Temp (t+1) [°C]']
+
 # Create the DTR object
 dtr = DTR(data)
 #dtr.compute_sensors_distance()
@@ -10,5 +16,16 @@ dtr.load_distances('./distances.csv')
 s1, s2 = dtr.get_furthest_sensors()
 
 
+# r1 = dtr.instance_based_transfer(s1, s2, inputs, output, target_num_available_days=25, target_num_test_days=30, sliding_window=True, regressor=RandomForestRegressor(n_jobs=-1), estimators_tradaboost=30, verbose=1)
+start = time.time()
+r2 = dtr.parameter_based_transfer(s1, s2, inputs, output, target_num_available_days=25, target_num_test_days=30, sliding_window=True, verbose=1, epochs=15)
 
+r3 = dtr.parameter_based_transfer(s2, s1, inputs, output, target_num_available_days=25, target_num_test_days=30, sliding_window=True, verbose=1, epochs=15)
 
+end = time.time()
+roundend = round(end - start, 2)
+print(roundend)
+
+# print(r1)
+print(r2)
+print(r3)
