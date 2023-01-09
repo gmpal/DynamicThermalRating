d714924b09802b24f6babdbca3262b97888cba79
gmpal <g.m.pal95@gmail.com>
2023-01-09 15:05:53 +0100

    compute distances

---
+++ dtr/compute_distances.py
@@ -0,0 +1,14 @@
+from dtr import DTR
+import pandas as pd
+
+
+data = pd.read_csv('./data.zip',index_col=0, infer_datetime_format=True, parse_dates=['datetime'])
+inputs = ['Wind Speed [m/s]', 'Arranged Wind Dir [°]', 'Air temp [°C]', 'Humidity [%]', 'Sun irradiance thermal flow absorbed by the conductor.[W/m]', 'Current flow [A]']
+output = ['Actual Conductor Temp (t+1) [°C]']
+
+
+dtr = DTR(data, inputs, output, n_jobs=30)
+
+dtr.compute_sensors_distance()
+
+dtr.export_distances('distances.csv')
